68ea596c92e73334526c08a37d09fea2384c7335
shakym <monyszkiewicz94@gmail.com>
2019-08-08 15:24:46 +0200

    Add project files.

---
+++ TestingSomeShit/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace TestingSomeShit
+{
+    public partial class Form1 : Form
+    {
+        static MongoClient client = new MongoClient("mongodb+srv://root:root@cluster0-uzqko.gcp.mongodb.net/test?retryWrites=true&w=majority");
+        static IMongoDatabase db = client.GetDatabase("movies");
+        static IMongoCollection<Movie> collection = db.GetCollection<Movie>("fantasy");
+
+        public void ReadAllDocuments()
+        {
+            List<Movie> list = collection.AsQueryable().ToList<Movie>();
+            dataGridView1.DataSource = list;
+            textBox1.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
+            this.dataGridView1.Columns["ID"].Visible = false;
+        }
+        public Form1()
+        {
+            
+
+            
+
+            InitializeComponent();
+            ReadAllDocuments();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Movie s = new Movie(textBox2.Text, Double.Parse(comboBox1.Text),textBox3.Text,textBox4.Text);
+            collection.InsertOne(s);
+            ReadAllDocuments();
+            /* var client = new MongoClient("mongodb://root:root@cluster0-uzqko.gcp.mongodb.net");*/
+            //var client = new MongoClient();
+
+
+
+
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var updateDef = Builders<Movie>.Update.Set("name", textBox2.Text).Set("type", textBox3.Text).Set("rate", comboBox1.Text).Set("description", textBox4.Text);
+            collection.UpdateOne(s => s.Id == ObjectId.Parse(textBox1.Text), updateDef);
+            ReadAllDocuments();
+        }
+    }
+}
---
+++ TestingSomeShit/Movie.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSomeShit
+{
+    class Movie
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public String name { get; set; }
+        [BsonElement("type")]
+        public String type { get; set; }
+        [BsonElement("rate")]
+        public double rate { get; set; }
+        [BsonElement("description")]
+        public string description { get; set; }
+
+
+        public Movie(string name, double rate, string type, string description)
+        {
+            this.name = name;
+            this.type = type;
+            this.description = description;
+            this.rate = rate;
+            
+        }
+    }
+}
